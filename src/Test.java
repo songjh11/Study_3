a9df6da34e737c0151be7177f0f0e54b8be80fcd
songjh11 <jhmelody91@gmail.com>
2022-07-06 12:10:25 +0900

    Study_3

--- src/Test.java
+++ src/Test.java
@@ -6,6 +6,9 @@ public class Test {
 System.out.println("git");
 System.out.println("git test");
 System.out.println("오늘 졸려 죽을것 같다");
+System.out.println("12시 점심시간에 너무 길들여졌나");
+System.out.println("커피끊을건데 미치겠네 진짜");
+System.out.println("오후에 조건문 하는데 정신차려야지");
 	}
 
 }
